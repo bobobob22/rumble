e8fb883fec02720852e2e86a79af0669c466a861
bobobob22 <bobobob22@users.noreply.github.com>
2020-09-10 21:50:01 +0000

    deploy: 4c1a05da15c28eb50536ae57864940df14bf2497

---
+++ precache-manifest.ad4ea6bf1406ff0ae9174cd57c84d7a6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0536b357f051f4d184f162e6f1d9ca50",
+    "url": "/rumble/index.html"
+  },
+  {
+    "revision": "c083a14c698e244feafe",
+    "url": "/rumble/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "a478c0075774602c0a7d",
+    "url": "/rumble/static/js/2.43dcd005.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/rumble/static/js/2.43dcd005.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c083a14c698e244feafe",
+    "url": "/rumble/static/js/main.87a3bf2d.chunk.js"
+  },
+  {
+    "revision": "d85e1040cb5fa5120ff8",
+    "url": "/rumble/static/js/runtime-main.6266cb62.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rumble/static/media/logo.5d5d9eef.svg"
+  }
+]);
