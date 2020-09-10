b967115617dcc6948a84baf5f6dddf3a19506589
Dawid Kwiatoń <dawidkwiaton123@gmail.com>
2020-09-10 23:14:48 +0200

    Update node.js.yml

--- rumble-app/src/App.tsx
+++ rumble-app/src/App.tsx
@@ -25,7 +25,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn Reactaa
         </a>
       </header>
     </div>
